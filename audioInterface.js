1ab2f5452823b660a6bb77db2425e6a6033d9fbc
bkeil <37550434+nuetu@users.noreply.github.com>
2022-11-21 00:25:22 -0500

    added audio interface shell

---
+++ audioInterface.js
@@ -0,0 +1,32 @@
+var main = document.getElementById("main");
+main.innerHTML = `<link rel="stylesheet" href="audioInterface.css" />
+        <div id="audioInterface">
+          <div class="aiSection">
+            <img class="aiKnob" src="dial.svg" alt="Audio Interface volume dial" />
+            <div class="aiBar"></div>
+            <div>
+              <button class="aiPhantom"></button><span class="pha">48V</span>
+            </div>
+          </div>
+          <div class="aiSection">
+          <img class="aiKnob" src="dial.svg" alt="Audio Interface volume dial" />
+            <div class="aiBar"></div>
+            <div>
+              <button class="aiPhantom"></button><span class="pha">48V</span>
+            </div>
+          </div>
+          <div class="aiSection">
+          <img class="aiKnob" src="dial.svg" alt="Audio Interface volume dial" />
+            <div class="aiBar"></div>
+            <div>
+              <button class="aiPhantom"></button><span class="pha">48V</span>
+            </div>
+          </div>
+          <div class="aiSection">
+          <img class="aiKnob" src="dial.svg" alt="Audio Interface volume dial" />
+            <div class="aiBar"></div>
+            <div>
+              <button class="aiPhantom"></button><span class="pha">48V</span>
+            </div>
+          </div>
+        </div>`;
